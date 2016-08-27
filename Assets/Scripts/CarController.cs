12efb3c597ee378e87363d9414343663473473c5
Aggrathon <antonbjo@gmail.com>
2016-08-27 12:09:47 +0300

    Car Controller

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using UnityEngine;
+
+namespace aggrathon.ld36
+{
+	[DisallowMultipleComponent]
+	[RequireComponent(typeof(Rigidbody))]
+	public class CarController : MonoBehaviour
+	{
+		public enum WheelDrive
+		{
+			FrontWheelDrive,
+			RearWheelDrive,
+			FourWheelDrive
+		}
+
+		[Tooltip("Requires exactly 4 wheels with the first 2 being the front wheels")]
+        [SerializeField] private WheelCollider[] wheelColliders = new WheelCollider[4];
+		[Tooltip("These visuals will be made to match the previous WheelColliders")]
+        [SerializeField] private GameObject[] wheelVisuals = new GameObject[4];
+		
+        [SerializeField] private float maximumSteerAngle = 20f;
+        [SerializeField] private float maximumTorque = 2500f;
+        [SerializeField] private float reverseTorque = 1500f;
+        [SerializeField] private float brakeTorque = 2000f;
+		[SerializeField] private WheelDrive driveType = WheelDrive.RearWheelDrive;
+        [SerializeField] [Range(1,2)] private float boostMultiplier = 1.2f;
+
+		[NonSerialized] public float accelerator = 0f;
+		[NonSerialized] public float steering = 0f;
+		[NonSerialized] public bool boosting = false;
+
+		private float currentTorque;
+		new private Rigidbody rigidbody;
+
+		void Start ()
+		{
+			rigidbody = GetComponent<Rigidbody>();
+		}
+
+		void FixedUpdate()
+		{
+			for (int i = 0; i < 4; i++)
+			{
+				Quaternion rot;
+				Vector3 pos;
+				wheelColliders[i].GetWorldPose(out pos, out rot);
+				wheelVisuals[i].transform.position = pos;
+				wheelVisuals[i].transform.rotation = rot;
+			}
+			
+			steering = Mathf.Clamp(steering, -1f, 1f);
+			wheelColliders[0].steerAngle = maximumSteerAngle * steering;
+			wheelColliders[1].steerAngle = maximumSteerAngle * steering;
+
+			if (accelerator > 0f)
+			{
+				if (accelerator > 1f)
+					accelerator = 1f;
+
+				if(rigidbody.velocity.sqrMagnitude > 6 && Vector3.Angle(transform.forward, rigidbody.velocity) > 90f)
+				{
+					wheelColliders[0].brakeTorque =
+						wheelColliders[1].brakeTorque =
+						wheelColliders[2].brakeTorque =
+						wheelColliders[3].brakeTorque = brakeTorque * accelerator;
+					currentTorque = 0f;
+				}
+				else
+				{
+					currentTorque = boosting ? maximumTorque * boostMultiplier : maximumTorque;
+					wheelColliders[0].brakeTorque =
+						wheelColliders[1].brakeTorque =
+						wheelColliders[2].brakeTorque =
+						wheelColliders[3].brakeTorque = 0f;
+				}
+			}
+			else
+			{
+				if (accelerator < -1f)
+					accelerator = -1f;
+
+				if (rigidbody.velocity.sqrMagnitude > 6 && Vector3.Angle(transform.forward, rigidbody.velocity) < 90f)
+				{
+					wheelColliders[0].brakeTorque =
+						wheelColliders[1].brakeTorque =
+						wheelColliders[2].brakeTorque =
+						wheelColliders[3].brakeTorque = -brakeTorque * accelerator;
+					currentTorque = 0f;
+				}
+				else
+				{
+					currentTorque = reverseTorque;
+					wheelColliders[0].brakeTorque =
+						wheelColliders[1].brakeTorque =
+						wheelColliders[2].brakeTorque =
+						wheelColliders[3].brakeTorque = 0f;
+				}
+			}
+
+			switch (driveType)
+			{
+				case WheelDrive.FrontWheelDrive:
+					wheelColliders[0].motorTorque =
+						wheelColliders[1].motorTorque = currentTorque * accelerator * 0.5f;
+					break;
+				case WheelDrive.RearWheelDrive:
+					wheelColliders[2].motorTorque =
+						wheelColliders[3].motorTorque = currentTorque * accelerator * 0.5f;
+					break;
+				case WheelDrive.FourWheelDrive:
+					wheelColliders[0].motorTorque =
+						wheelColliders[1].motorTorque =
+						wheelColliders[2].motorTorque =
+						wheelColliders[3].motorTorque = currentTorque * accelerator * 0.25f;
+					break;
+			}
+		}
+
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace aggrathon.ld36
+{
+	public class PlayerController : MonoBehaviour
+	{
+		[SerializeField] private aggrathon.ld36.CarController car;
+
+		void Update()
+		{
+			car.steering = Input.GetAxis("Horizontal");
+			car.accelerator = Input.GetAxis("Vertical");
+		}
+	}
+}
