463b61d7c0bf6d8ecd9d2c3065e21ff59347096e
Aggrathon <antonbjo@gmail.com>
2016-08-27 12:26:31 +0300

    Controller Support

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,12 +5,36 @@ namespace aggrathon.ld36
 {
 	public class PlayerController : MonoBehaviour
 	{
+		enum InputType
+		{
+			Both,
+			Keyboard,
+			Controller
+		}
+
 		[SerializeField] private aggrathon.ld36.CarController car;
+		[SerializeField] private InputType controller;
 
 		void Update()
 		{
-			car.steering = Input.GetAxis("Horizontal");
-			car.accelerator = Input.GetAxis("Vertical");
+			switch (controller)
+			{
+				case InputType.Keyboard:
+					car.steering = Input.GetAxis("P1 Horizontal");
+					car.accelerator = Input.GetAxis("P1 Vertical");
+					car.boosting = Input.GetButton("P1 Boost");
+					break;
+				case InputType.Controller:
+					car.steering = Input.GetAxis("P2 Horizontal");
+					car.accelerator = Input.GetAxis("P2 Vertical");
+					car.boosting = Input.GetButton("P2 Boost");
+					break;
+				case InputType.Both:
+					car.steering = Input.GetAxis("Horizontal");
+					car.accelerator = Input.GetAxis("Vertical");
+					car.boosting = Input.GetButton("P1 Boost") || Input.GetButton("P2 Boost");
+					break;
+			}
 		}
 	}
 }
